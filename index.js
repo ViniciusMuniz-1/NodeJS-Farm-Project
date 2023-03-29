e7f216a545429dc3244aaca976c4b8271394965d
José Vinicius Targino Muniz <20222014040010@ifrn.local>
2023-03-29 10:57:33 -0300

    Creating a simple server and consuming the api

--- index.js
+++ index.js
@@ -1,8 +1,63 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
+//////////////////////////////////////////////
+//FILES
 
-const textOut = `This is what we know abou avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log("File written.")
+// Blocking, synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+// const textOut = `This is what we know abou avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log("File written.")
+
+// Non Blocking, asynchronous way
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     if (err) return console.log("ERROR")
+
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {  
+//             console.log(data3);
+
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//                 console.log('Your file has been written 😊')
+//             });
+//         });
+//     });
+// });
+
+// console.log("Will read file.")
+
+////////////////////////////////////////////////////
+//SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const productData = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+    const pathName = req.url;
+
+    if (pathName === '/' || pathName === '/overview'){
+        res.end("This is overview.");
+    }
+    else if (pathName === '/product'){
+        res.end("This is the product.");
+    }
+    else if (pathName === '/api'){
+        res.writeHead(200, {'Content-type': 'application/json'});
+        res.end(data);
+    }
+    else{
+        res.writeHead(404,{
+            'Content-type': 'text/html',
+            'my-own-header' : 'hello-world'
+        });
+        res.end("<h1>Page not found!</h1>");
+    }
+});
+
+server.listen(8081, '127.0.0.1', () => {
+    console.log("Server running in port 8081.");
+});
